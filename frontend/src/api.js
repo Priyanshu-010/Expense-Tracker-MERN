f618d71240d995659f86074b8243b2400aa81c78
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-09-12 23:09:20 +0530

    Deployed backend URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:3000/api' });
+const API = axios.create({ baseURL: 'https://expense-tracker-mern-backend-2.onrender.com/api' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('token')) {
